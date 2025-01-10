d6f7bc397b71329987eae707b6d5fb38754aafab
Pham Vuong Hoang Nhat <phamvuonghoangnhat@gmail.com>
2025-01-10 18:01:10 +0700

    fix build api

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,8 +2,6 @@ import { createContext } from "@/server/context";
 import { appRouter } from "@/server/routers/_app";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-export const dynamic = "force-dynamic";
-
 export default createNextApiHandler({
   router: appRouter,
   createContext,
