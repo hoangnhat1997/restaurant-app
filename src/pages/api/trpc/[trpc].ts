acf30428edc093739ce0627dd530f0c7961dc51c
Pham Vuong Hoang Nhat <phamvuonghoangnhat@gmail.com>
2025-01-10 16:10:48 +0700

    fix server issue

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,9 +1,10 @@
+import { createContext } from "@/server/context";
 import { appRouter } from "@/server/routers/_app";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
 export default createNextApiHandler({
   router: appRouter,
-  createContext: () => ({}),
+  createContext,
   onError({ error }) {
     if (error.code === "INTERNAL_SERVER_ERROR") {
       console.error("Something went wrong", error);
