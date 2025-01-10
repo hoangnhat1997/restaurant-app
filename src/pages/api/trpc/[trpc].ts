2a67156efeeb7d5b7d6b0e62840ad728fba59c06
Pham Vuong Hoang Nhat <phamvuonghoangnhat@gmail.com>
2025-01-10 16:43:39 +0700

    fix render api

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,6 +2,8 @@ import { createContext } from "@/server/context";
 import { appRouter } from "@/server/routers/_app";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
+export const dynamic = "force-dynamic";
+
 export default createNextApiHandler({
   router: appRouter,
   createContext,
